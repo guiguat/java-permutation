b389d7a9d5dc7cab7ff85634d1b0f2e301093542
guiguat <guaturadzn@gmail.com>
2021-01-23 18:59:19 -0300

    refactor "ABC" to var word

--- src/com/github/guiguat/wordlist/Main.java
+++ src/com/github/guiguat/wordlist/Main.java
@@ -3,14 +3,16 @@ package com.github.guiguat.wordlist;
 public class Main {
 
     public static void main(String[] args) {
+        String word = "ABCDE";
+
         long startTime = System.nanoTime();
-        Permutation.of("ABC").build();
+        Permutation.of(word).build();
         long endTime = System.nanoTime();
         long duration = (endTime - startTime);
         System.out.printf("\nExecuted in: %.2f seconds\n",duration/1e9);
 
         long startTimeParallel = System.nanoTime();
-        Permutation.of("ABC").buildParallel(4);
+        Permutation.of(word).buildParallel(4);
         long endTimeParallel = System.nanoTime();
         long durationParallel = (endTimeParallel - startTimeParallel);
         System.out.printf("\nExecuted in: %.2f seconds in parallel\n",durationParallel/1e9);
